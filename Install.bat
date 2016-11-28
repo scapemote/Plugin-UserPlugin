024dec61d562e69b23a0224d7469736ca8ea52ba
droidscript.org <DroidScript@users.noreply.github.com>
2016-11-28 00:45:36 +0000

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,4 +1,4 @@
 
-C:\SDKs\Android3\platform-tools\adb push "app/build/outputs/apk/MyPlugin.apk" "/sdcard/DroidScript/Plugins/MyPlugin.apk"
+C:\SDKs\Android3\platform-tools\adb push "app/build/outputs/apk/MyPluginrelease.apk" "/sdcard/DroidScript/Plugins/MyPlugin.apk"
 
 pause 
